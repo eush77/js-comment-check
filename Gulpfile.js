9cc179287a7cee7dec4e52061fd345f578b5ca92
Eugene Sharygin <eush77@gmail.com>
2014-08-23 17:03:05 +0400

    Build with Gulp

---
+++ Gulpfile.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var gulp = require('gulp')
+  , jshint = require('gulp-jshint')
+  , mocha = require('gulp-mocha');
+
+
+gulp.task('lint', function () {
+  return gulp.src(['src/**/*.js', 'test/**/*.js', 'Gulpfile.js'])
+             .pipe(jshint())
+             .pipe(jshint.reporter('jshint-stylish'))
+             .pipe(jshint.reporter('fail'));
+});
+
+
+gulp.task('test', function () {
+  require('should');
+  return gulp.src('test/**/*.js')
+             .pipe(mocha({
+               reporter: 'spec'
+             }));
+});
+
+
+gulp.task('default', ['lint', 'test']);
